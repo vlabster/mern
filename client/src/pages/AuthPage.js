5ed234daf4e681357a0521dd56472b3cbb1b1493
vlabster <vlabster@mail.ru>
2021-06-09 22:52:46 +0300

    Update AuthPage.js

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -3,7 +3,23 @@ import React from 'react'
 export const AuthPage = () => {
     return (
         <div>
-            <h1>Auth Page</h1>
+            <div className="row">
+                <div className="col s6 offset-s3">
+                    <div className="card blue darken-1">
+                        <div className="card-content white-text">
+                        <span className="card-title">Сократи ссылку</span>
+                            <p>
+                                I am a very simple card. I am good at containing small bits of information. 
+                                I am convenient because I require little markup to use effectively.
+                            </p>
+                        </div>
+                        <div className="card-action">
+                        <a href="#">This is a link</a>
+                        <a href="#">This is a link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
