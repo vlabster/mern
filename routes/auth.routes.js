15b6afe7b62335d32232de995a1a3988c7a1add9
vlabster <vlabster@mail.ru>
2021-06-09 17:44:04 +0300

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,27 @@
 const { Router } = require('express')
+const User = require('../models/User')
 const router = new Router()
+
+// /api/auth/register
+router.post('/register', async(req, res) => {
+    try {
+        const { email, password } = req.body
+
+        const candidate = await User.findOne({ email })
+
+        if (candidate) {
+            res.status(400).json({ message: 'Пользователь с таким email уже зарегистрирован' })
+            return
+        }
+
+    } catch (e) {
+        res.status(500).json({ message: 'Что-то пошло не так, попробуйте ещё раз' })
+    }
+})
+
+// /api/auth/login
+router.post('/login', async(req, res) => {
+
+})
 
 module.exports = router
