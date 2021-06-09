30be0177be1ac5fac8b5aa0f378fe06c5d687619
vlabster <vlabster@mail.ru>
2021-06-09 17:25:58 +0300

    add mongoDb connection

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const PORT = config.get('port') || 5000
+
+async function start() {
+    try {
+
+        //DB connection
+        await mongoose.connect(config.get('mongo_url'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+
+        //Start server
+        app.listen(PORT, () => {
+            console.log(`Hello world on ${PORT} port.`)
+        })
+
+    } catch (e) {
+        console.log('Server error!', e.message)
+        process.exit(1)
+    }
+}
+
+
+
+start()
