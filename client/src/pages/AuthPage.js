08194c3b30395e4e166cd856535e12c9e4512e01
vlabster <vlabster@mail.ru>
2021-06-10 15:37:07 +0300

    use hook

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,21 +1,90 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useHttp} from '../hooks/http.hook'
 
 export const AuthPage = () => {
+
+    const { loading, error, request } = useHttp()
+
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    })
+
+    const changeHandler = event => {
+        setForm({ ...form, [event.target.name]: event.target.value })
+    }
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/register', 'POST', { ...form })
+            console.log(`Data: ${data}`)
+        }
+        catch(e) {
+            
+        }
+    }
+
+    const loginHandler = async () => {
+        try {
+            const data = await request('/api/auth/login', 'POST', { ...form })
+            console.log(`Data: ${data}`)
+        }
+        catch(e) {
+
+        }
+    }
+
     return (
         <div>
             <div className="row">
                 <div className="col s6 offset-s3">
+                    <h1>Сократи ссылку</h1>
                     <div className="card blue darken-1">
                         <div className="card-content white-text">
-                        <span className="card-title">Сократи ссылку</span>
-                            <p>
-                                I am a very simple card. I am good at containing small bits of information. 
-                                I am convenient because I require little markup to use effectively.
-                            </p>
+                            <span className="card-title">Авторизация</span>
+                            <div>
+
+                                <div className="input-field">
+                                    <input 
+                                        placeholder="Введите email"
+                                        id="email"
+                                        type="text"
+                                        name="email"
+                                        className="yellow-input"
+                                        onChange={changeHandler}
+                                    />
+                                    <label htmlFor="email">Email</label>
+                                </div>
+
+                                <div className="input-field">
+                                    <input 
+                                        placeholder="Введите пароль"
+                                        id="password"
+                                        type="password"
+                                        name="password"
+                                        className="yellow-input"
+                                        onChange={changeHandler}
+                                    />
+                                    <label htmlFor="password">Пароль</label>
+                                </div>
+                            </div>
                         </div>
                         <div className="card-action">
-                        <a href="#">This is a link</a>
-                        <a href="#">This is a link</a>
+                            <button 
+                                className="btn yellow darken-4" 
+                                style={{marginRight: 10}}
+                                onClick={loginHandler}
+                                disabled={loading}
+                            >
+                                Войти
+                            </button>
+                            <button 
+                                className="btn grey lighten-1 black-text"
+                                onClick={registerHandler}
+                                disabled={loading}
+                            >
+                                Регистрация
+                            </button>
                         </div>
                     </div>
                 </div>
