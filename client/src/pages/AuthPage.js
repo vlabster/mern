f6888781ef09382814ed13572e1247be09031fa7
vlabster <vlabster@mail.ru>
2021-06-11 16:50:34 +0300

    use auth context

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,11 @@
-import React, {useEffect, useState} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import {useHttp} from '../hooks/http.hook'
 import {useMessage} from '../hooks/message.hook'
+import { AuthContext } from '../context/AuthContext'
 
 export const AuthPage = () => {
 
+    const auth = useContext(AuthContext)
     const message = useMessage()
 
     const { loading, error, request, clearError } = useHttp()
@@ -37,7 +39,7 @@ export const AuthPage = () => {
     const loginHandler = async () => {
         try {
             const data = await request('/api/auth/login', 'POST', { ...form })
-            message(data.message)
+            auth.login(data.token, data.userId)
         }
         catch(e) {
 
