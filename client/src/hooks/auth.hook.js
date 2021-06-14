2397bb2c9bfd28400f65dfd266712a8101854b44
vlabster <vlabster@mail.ru>
2021-06-14 17:51:15 +0300

    Update auth.hook.js

--- client/src/hooks/auth.hook.js
+++ client/src/hooks/auth.hook.js
@@ -5,6 +5,7 @@ const storageName = 'userData'
 export const useAuth = () => {
 
     const [token, setToken] = useState(null)
+    const [ready, setReady] = useState(false)
     const [userId, setUserId] = useState(null)
 
     const login = useCallback((jwtToken, id) => {
@@ -33,7 +34,8 @@ export const useAuth = () => {
         if (data && data.token) {
             login(data.token, data.userId)
         }
+        setReady(true)
     }, [login])
 
-    return { login, logout, token, userId }
+    return { login, logout, token, userId, ready }
 }   
