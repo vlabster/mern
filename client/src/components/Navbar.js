8dea7b599b69118f782e5c549ab05882c237650a
vlabster <vlabster@mail.ru>
2021-06-14 16:54:42 +0300

    fixed className

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -16,9 +16,9 @@ export const Navbar = () => {
 
     return (
         <nav>
-            <div class="nav-wrapper blue darken-1" style={{ padding: '0 2rem' }}>
-            <span class="brand-logo">Сокращение ссылок</span>
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <div className="nav-wrapper blue darken-1" style={{ padding: '0 2rem' }}>
+            <span className="brand-logo">Сокращение ссылок</span>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><NavLink to="/create">Создать</NavLink></li>
                 <li><NavLink to="/links">Список</NavLink></li>
                 <li><a href="/" onClick={logoutHandler}>Выйти</a></li>
