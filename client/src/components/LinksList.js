3b6a578becbf6bb3862cd1e8099ad4d6efe80de1
vlabster <vlabster@mail.ru>
2021-06-16 14:23:25 +0300

    add Link List component

---
+++ client/src/components/LinksList.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const LinksList = ({ links }) => {
+
+    if (!links.length) {
+        return <p classame="center">Ссылки отсутствуют</p>
+    }
+
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>№</th>
+                <th>Оригинальная ссылка</th>
+                <th>Сокращённая ссылка</th>
+                <th>Открыть</th>
+            </tr>
+            </thead>
+
+            <tbody>
+                { links.map((link, index) => {
+                    return (
+                        <tr key={link._id}>
+                            <td>{ index + 1 }</td>
+                            <td>{ link.from }</td>
+                            <td>{ link.to }</td>
+                            <td>
+                                <Link to={`/detail/${link._id}`}>Открыть</Link>
+                            </td>
+                        </tr>
+                    )
+                }) }
+            </tbody>
+        </table>
+    )
+}
