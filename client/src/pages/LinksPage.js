7e649fcbe5273576dee21764651af2047858a06a
vlabster <vlabster@mail.ru>
2021-06-17 16:03:06 +0300

    add link delete button

--- client/src/pages/LinksPage.js
+++ client/src/pages/LinksPage.js
@@ -3,14 +3,34 @@ import { useHttp } from '../hooks/http.hook'
 import { AuthContext } from '../context/AuthContext'
 import { Loader } from '../components/Loader'
 import { LinksList } from '../components/LinksList'
+import {useMessage} from '../hooks/message.hook'
 
 export const LinksPage = () => {
+
+    const message = useMessage()
 
     const [links, setLinks] = useState([])
 
     const { loading, request } = useHttp()
 
     const { token, logout } = useContext(AuthContext)
+
+    const deleteLink = async (event, id) => {
+        event.preventDefault()
+        try {
+            const fetched = await request(`/api/link/${id}`, 'DELETE', null, { Authorization: `Bearer ${token}` })
+            console.log(fetched)
+            setLinks(links.filter(item => item._id !== id))
+        }
+        catch(e) {
+            if (e.message === 'Нет авторизации') {
+                logout()
+                return
+            }
+            message('Ошибка удаления ссылки')
+        }
+        
+    }
 
     const fetchLinks = useCallback( async () => {
 
@@ -36,7 +56,7 @@ export const LinksPage = () => {
 
     return (
         <>
-            {!loading && <LinksList links={links} />} 
+            {!loading && <LinksList links={links} deleteLink={deleteLink}/>} 
         </>
     )
 }
